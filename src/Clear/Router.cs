ac81f9c38c8fff03d3a9c255270b96ef1c2895bf
Alan Mervitz <amervitz@users.noreply.github.com>
2018-12-19 21:59:50 -0600

    refactoring, improve method matching
    Only match on the method if all the arguments get mapped to a parameter successfully

--- src/Clear/Router.cs
+++ src/Clear/Router.cs
@@ -10,22 +10,17 @@ namespace Clear
     {        
         public static object Run(Type type, string[] args)
         {
-            var methodFinder = new SingleMethodFinder(type);
+            var methodFinder = new FirstMethodFinder(type);
 
             var command = args.Length > 0 ? args[0] : null;
 
-            if (command != null && methodFinder.TryFindMethodToExecute(command, out var methodToExecute))
+            if (command != null)
             {
-                var methodParameters = methodToExecute.GetParameters();
-
-                var commandArguments = args.Skip(1).ToArray();
-
-                var (success, parametersToSupply) = TryGetParameterValues(methodParameters, commandArguments);
-
-                if(success && parametersToSupply.Length == methodParameters.Length)
+                var (method, parameters) = methodFinder.TryFindMethodToInvoke(command, args.Skip(1).ToArray());
+                if (method != null)
                 {
-                    var returnVal = methodToExecute.Invoke(null, parametersToSupply);
-                    return returnVal;
+                    var returnValue = method.Invoke(null, parameters);
+                    return returnValue;
                 }
             }
 
@@ -91,68 +86,6 @@ namespace Clear
             );
 
             return classToExecute != null;
-        }
-
-        private static (bool, object[]) TryGetParameterValues(ParameterInfo[] parameterTypes, string[] args)
-        {
-            var parameterValues = new List<(bool?, object)>(parameterTypes.Length);
-            parameterValues.AddRange(Enumerable.Repeat<(bool?, object)>((default, null), parameterTypes.Length));
-            
-            var parameterConverter = new ParameterTypeConverter();
-            for (int i = 0; i < args.Length && parameterValues.FindIndex(pv => pv.Item1 == null) >= 0; i++)
-            {
-                // get the parameter value
-                var arg = args[i];
-
-                // check if it's a named argument
-                var namedArgument = parameterTypes.FirstOrDefault(p => string.Equals($"--{p.Name}", arg, StringComparison.InvariantCultureIgnoreCase));
-
-                // if a named argument, add the next value as the parameter value
-                if (namedArgument != null)
-                {
-                    var paramIndex = Array.IndexOf(parameterTypes, namedArgument);
-                    if(args.Length >= i + 2)
-                    {
-                        // only convert and assign the value if it hasn't already been assigned, e.g. by an unnamed argument
-                        if (parameterValues[paramIndex].Item1 == null)
-                        {
-                            parameterValues[paramIndex] = parameterConverter.TryChangeValue(args[i + 1], namedArgument.ParameterType);
-                        }
-
-                        i += 1;
-                    }
-                    else // the last parameter and it has a default value
-                    {
-                        if (namedArgument.HasDefaultValue)
-                        {
-                            parameterValues[i] = (true, namedArgument.DefaultValue);
-                        }
-                    }
-                }
-                // try to use the current argument as the next parameter value 
-                else
-                {
-                    // find the first null position
-                    var emptyIndex = parameterValues.FindIndex(pv => pv.Item1 == null);
-
-                    // get the named argument for the position
-                    var unnamedArgument = parameterTypes[emptyIndex];
-                    parameterValues[emptyIndex] = parameterConverter.TryChangeValue(arg, unnamedArgument.ParameterType);
-                }
-            }
-
-            // assign any default values to unassigned parameters
-            for (int i = 0; i < parameterTypes.Length; i++)
-            {
-                if (parameterValues[i].Item1 == null && parameterTypes[i].HasDefaultValue)
-                {
-                    parameterValues[i] = (true, parameterTypes[i].DefaultValue);
-                }
-            }
-
-            var allParsedSuccessfully = parameterValues.All(p => p.Item1 == true);
-
-            return (allParsedSuccessfully, parameterValues.Select(i => i.Item2).ToArray());
         }
     }
 }
